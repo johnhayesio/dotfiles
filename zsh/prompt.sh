fca75fa9f7b05a6ada24c531a2628227c0de11c6
John Hayes <johnhayesru@gmail.com>
2018-12-01 21:19:49 +0300

    fixed bolding within prompt

--- zsh/prompt.sh
+++ zsh/prompt.sh
@@ -7,11 +7,11 @@ setopt PROMPT_SUBST
 set_prompt() {
 
 	# [
-	PS1="%{$fg[white]%}[%{$reset_color%}"
+	PS1="%{$fg_bold[white]%}[%{$reset_color%}"
 
 	# Path: http://stevelosh.com/blog/2010/02/my-extravagant-zsh-prompt/
 	# PS1+="%{$fg_bold[cyan]%}${PWD/#$HOME/λ}%{$reset_color%}"
-	PS1+="$fg[green]λ"
+	PS1+="%{$fg_bold[green]%}λ%{$reset_color%}"
 
 	# Status Code
 	PS1+='%(?.., %{$fg[red]%}%?%{$reset_color%})'
@@ -19,7 +19,7 @@ set_prompt() {
 	# Git
 	if git rev-parse --is-inside-work-tree 2> /dev/null | grep -q 'true' ; then
 		PS1+=', '
-		PS1+="%{$fg[blue]%}$(git rev-parse --abbrev-ref HEAD 2> /dev/null)%{$reset_color%}"
+		PS1+="%{$fg_bold[blue]%}$(git rev-parse --abbrev-ref HEAD 2> /dev/null)%{$reset_color%}"
 		if [ $(git status --short | wc -l) -gt 0 ]; then 
 			PS1+="%{$fg[red]%}+$(git status --short | wc -l | awk '{$1=$1};1')%{$reset_color%}"
 		fi
@@ -46,7 +46,7 @@ set_prompt() {
 		PS1+="%{$fg_bold[red]%}SUDO%{$reset_color%}"
 	fi
 
-	PS1+="%{$fg[white]%}]: %{$reset_color%}% "
+	PS1+="%{$fg_bold[white]%}]: %{$reset_color%}% "
 }
 
 precmd_functions+=set_prompt
