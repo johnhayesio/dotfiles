9b036f1f40caef8fe62c405efb605f51cadf72fb
John H <johnhayesru@gmail.com>
2019-12-17 01:11:02 +0300

    Changed zsh prompt.

--- zsh/prompt.sh
+++ zsh/prompt.sh
@@ -10,7 +10,7 @@ set_prompt() {
 	PS1="%{$fg_bold[white]%}[%{$reset_color%}"
 
 	# Path: http://stevelosh.com/blog/2010/02/my-extravagant-zsh-prompt/
-	# PS1+="%{$fg_bold[green]%}${PWD/#$HOME/λ}%{$reset_color%}"
+  #	PS1+="%{$fg[blue]%* $fg_bold[green]%}${PWD/#$HOME/λ}%{$reset_color%}"
   PS1+="%{$fg_bold[green]%}λ%{$reset_color%}"
 
 	# Status Code
