04a38c5f92bcd89ce0a18fbb38a88b5918c3a50f
John H <johnhayesru@gmail.com>
2020-04-13 05:00:16 -0500

    added FZF_DEFAULT_OPTS, FZF_CTRL_T_COMMAND, and FZF_CTRL_ALT_C_COMMAND

--- zsh/zshrc.sh
+++ zsh/zshrc.sh
@@ -9,8 +9,11 @@ neofetch
 # Settings
   export VISUAL=vim
   export EDITOR=vim
+  export FD_OPTIONS='--follow --exclude .git --exclude node_modules'
   export FZF_DEFAULT_COMMAND='rg --files --no-ignore --hidden --follow --glob "!node_modules" --glob "!.git" --glob "!.meteor"'
-  export FZF_DEFAULT_OPTS="--ansi --preview-window 'right:60%' --preview 'bat --color=always --style=header,grid --line-range :300 {}'"
+  export FZF_DEFAULT_OPTS="--no-mouse --height 50% -1 --reverse --multi --inline-info --preview='[[ \$(file --mime {}) =~ binary ]] && echo {} is a binary file || (bat --style=numbers --color=always {} || cat {}) 2> /dev/null | head -300' --preview-window='right:hidden:wrap' --bind='f3:execute(bat --style=numbers {} || less -f {}),f2:toggle-preview,ctrl-d:half-page-down,ctrl-u:half-page-up,ctrl-a:select-all+accept,ctrl-y:execute-silent(echo {+} | pbcopy)'"
+  export FZF_CTRL_T_COMMAND='fd $FD_OPTIONS'
+  export FZF_CTRL_ALT_C_COMMAND='fd --type d $FD_OPTIONS'
   source ~/dotfiles/zsh/plugins/fixls.zsh
 
 # Aliases
