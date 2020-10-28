f558fa649035d8d0e14737d73f41a3fed3df186e
John H <johnhayesru@gmail.com>
2020-10-27 21:35:58 -0500

    added checking for updates of dotfiles to zshrc_manager

--- config/zsh/zshrc_manager.sh
+++ config/zsh/zshrc_manager.sh
@@ -7,6 +7,17 @@ time_out () { perl -e 'alarm shift; exec @ARGV' "$@"; }
 #	echo "tmux not installed. Run ./deploy to configure dependencies"
 #fi
 
-#(cd ~/dotfiles && time_out 3 git pull && time_out 3 git submodule update --init --recursive)
-#(cd ~/dotfiles && git pull && git submodule update --init --recursive)
+echo "Checking for updates."
+({cd ~/dotfiles && git fetch -q} &> /dev/null)
+
+if [ $({cd ~/dotfiles} &> /dev/null && git rev-list HEAD...origin/master | wc -l) = 0 ]
+then
+	echo "Already up to date."
+else
+	echo "Updates Detected:"
+	({cd ~/dotfiles} &> /dev/null && git log ..@{u} --pretty=format:%Cred%aN:%Creset\ %s\ %Cgreen%cd)
+	echo "Setting up..."
+	({cd ~/dotfiles} &> /dev/null && git pull -q && git submodule update --init --recursive)
+fi
+
 source ~/dotfiles/config/zsh/zshrc.sh
