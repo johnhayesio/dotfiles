08a49806a0f658ca7dc101617f27275ce7d6bdeb
John H <johnhayesru@gmail.com>
2020-10-30 08:30:20 -0500

    changed zsh_history size to 500000

--- config/zsh/zshrc.sh
+++ config/zsh/zshrc.sh
@@ -10,7 +10,8 @@ export NVM_DIR="$HOME/.nvm"
 
 # Vars
 	HISTFILE=~/.zsh_history
-	SAVEHIST=10000
+  HISTSIZE=500000
+	SAVEHIST=500000
 	setopt inc_append_history # To save every command before it is executed
 	setopt share_history # setopt inc_append_history
 
