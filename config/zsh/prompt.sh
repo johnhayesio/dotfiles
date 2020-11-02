fcd95b18c98ff7ca306f30833d23823d466ab8af
John H <johnhayesru@gmail.com>
2020-11-01 19:11:30 -0600

    added header and AGPL license to custom prompt

--- config/zsh/prompt.sh
+++ config/zsh/prompt.sh
@@ -1,3 +1,24 @@
+################################################################################
+# Zsh Custom Prompt
+# A custom zsh prompt showing sudo, jobs, git status, error codes, and
+# execution times in a super friendly layout
+################################################################################
+
+# Licensed under the AGPL version 3
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 source $HOME/dotfiles/config/zsh/jobs-pro.sh
 source $HOME/dotfiles/config/zsh/git-pro.sh
 
